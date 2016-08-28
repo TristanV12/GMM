c02d3125dd9a977267795b7ca42cbc37c7ee08b9
TristanV12 <villamil101@gmail.com>
2016-08-28 18:00:09 -0400

    Fix file placement

--- generateEGMMTest.py
+++ generateEGMMTest.py
@@ -2,9 +2,12 @@ from generate import *
 import csv
 import sys
 import glob
+import os
 
 def generateTrial(trial, rankings):
-	file1 = open('data\\M6K2Trial' + str(trial) + '.csv', 'a')
+	filename = '/data/M6K2Trial' + str(trial) + '.csv'
+	os.makedirs(os.path.dirname(filename), exist_ok=True)
+	file1 = open(filename, 'a')
 	parameters = GenerateRUMParameters(6, "normal")
 	arr = GenerateRUMData(parameters, 6, rankings, "normal")
 	spamwriter = csv.writer(file1, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
